d71d937778934a3341c350bd86c95f99f4510404
Milosz Mackowski-Nowak <miloszmn7@gmail.com>
2026-01-22 00:34:30 +0100

    Sceneria i rece, teleportacja i chodzenie

--- project_files/movement.gd
+++ project_files/movement.gd
@@ -1,40 +1,43 @@
 extends CharacterBody3D
 
 @export var move_speed: float = 3.0
-@export var deadzone: float = 0.3 # Zwiększone dla bezpieczeństwa
+@export var deadzone: float = 0.3 
 
-# Ścieżki
+# Ścieżki do wnętrza gracza
 @onready var xr_origin: XROrigin3D = $XROrigin3D
 @onready var xr_camera: XRCamera3D = $XROrigin3D/XRCamera3D
+# Ruch zazwyczaj jest na Lewym Kontrolerze
 @onready var left_controller: XRController3D = $XROrigin3D/LeftController
 
-# Referencje do Twojego ciała (Kapsuła + Czerwony Walec)
+# Referencje do kolizji (do centrowania)
 @onready var collision_shape: CollisionShape3D = $CollisionShape3D
-# Jeśli nazwałeś czerwony walec "MeshInstance3D", zostaw tak. Jeśli inaczej, popraw nazwę!
+# Jeśli dodałeś czerwony walec, wpisz tu jego nazwę (np. MeshInstance3D)
 @onready var body_mesh: MeshInstance3D = $MeshInstance3D 
 
 func _physics_process(_delta: float) -> void:
-	# 1. Input z kontrolerów
+	# 1. INPUT (VR + Klawiatura)
 	var input_vector = Vector2.ZERO
 	if left_controller:
 		input_vector = left_controller.get_vector2("thumbstick")
+		# Obsługa alternatywnej nazwy osi (dla pewności)
 		if input_vector.length() == 0:
 			input_vector = left_controller.get_vector2("primary")
 
-	# Input z klawiatury (PC Debug)
+	# Obsługa klawiatury (jeśli nie masz gogli na głowie)
 	if input_vector.length() < deadzone:
 		input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 
-	# 2. Ruch i Hamowanie
+	# 2. RUCH I HAMOWANIE
 	if input_vector.length() < deadzone:
+		# Hamowanie (tarcie)
 		velocity.x = move_toward(velocity.x, 0, move_speed)
 		velocity.z = move_toward(velocity.z, 0, move_speed)
 	else:
-		# Obliczanie kierunku ruchu
+		# Obliczanie kierunku wg patrzenia kamery
 		var forward_dir = -xr_camera.global_transform.basis.z
 		var right_dir = xr_camera.global_transform.basis.x
 		
-		# Spłaszczanie (żeby nie latać)
+		# Spłaszczanie (blokada latania)
 		forward_dir.y = 0.0
 		right_dir.y = 0.0
 		forward_dir = forward_dir.normalized()
@@ -46,25 +49,23 @@ func _physics_process(_delta: float) -> void:
 			velocity.x = direction.x * move_speed
 			velocity.z = direction.z * move_speed
 
-	# 3. FIZYKA (To przesuwa gracza)
+	# 3. FIZYKA (Przesuwa CharacterBody3D)
 	move_and_slide()
 	
-	# 4. NOWE CENTROWANIE (Wywoływane PO ruchu fizyki)
+	# 4. AUTO-CENTROWANIE (Najważniejsza poprawka)
+	# Przesuwamy kapsułę pod głowę, ale NIE ruszamy Origina
 	_recenter_collider_only()
 
-# --- NOWA FUNKCJA (BEZ PĘTLI SPRZĘŻENIA) ---
 func _recenter_collider_only() -> void:
-	# Pobieramy pozycję kamery w przestrzeni lokalnej gracza
-	# To nam mówi: "Gdzie jest głowa względem środka pokoju (0,0)?"
+	# Gdzie jest głowa w przestrzeni lokalnej gracza?
 	var head_local_pos = to_local(xr_camera.global_position)
 	
-	# Interesuje nas tylko X i Z (podłoga)
-	var target_pos = Vector3(head_local_pos.x, 0.9, head_local_pos.z) # 0.9 to wysokość środka kapsuły
+	# Ignorujemy wysokość głowy, interesuje nas podłoga (X, Z)
+	# 0.9 to środek wysokości kapsuły (dla kapsuły 1.8m)
+	var target_pos = Vector3(head_local_pos.x, 0.9, head_local_pos.z)
 	
-	# Przesuwamy samą kapsułę kolizyjną w to miejsce
+	# Przesuwamy elementy fizyczne i graficzne
 	if collision_shape:
 		collision_shape.position = target_pos
-		
-	# Przesuwamy też czerwony walec, żebyś widział, gdzie jest ciało
 	if body_mesh:
 		body_mesh.position = target_pos
--- project_files/teleport.gd
+++ project_files/teleport.gd
@@ -1,70 +1,68 @@
 extends XRController3D
 
-# Referencje do węzłów (upewnij się, że nazwy w drzewie są identyczne: TeleportRay, TeleportMarker)
+# Upewnij się, że masz te węzły jako dzieci kontrolera!
 @onready var ray: RayCast3D = $TeleportRay
 @onready var marker: MeshInstance3D = $TeleportMarker
 
 var xr_origin: XROrigin3D
 var xr_camera: XRCamera3D
-var is_aiming := false  # Flaga: czy aktualnie celujemy?
-
-# Ustawienie czułości gałki
+var is_aiming := false
 const DEADZONE = 0.2
 
 func _ready() -> void:
-	# Pobieramy XROrigin (rodzica kontrolera) i Kamerę
-	xr_origin = get_parent() as XROrigin3D
-	xr_camera = xr_origin.get_node("XRCamera3D") as XRCamera3D
-	
-	# Na starcie ukrywamy znacznik i wyłączamy laser (oszczędność wydajności)
-	marker.visible = false
-	ray.enabled = false 
+    xr_origin = get_parent() as XROrigin3D
+    xr_camera = xr_origin.get_node("XRCamera3D") as XRCamera3D
+    
+    # Start: ukryte
+    marker.visible = false
+    ray.enabled = false 
 
 func _process(_delta: float) -> void:
-	var input_vector = get_vector2("thumbstick")
-	
-	# --- 1. CELOWANIE ---
-	if input_vector.length() > DEADZONE:
-		if not is_aiming:
-			is_aiming = true
-			ray.enabled = true 
-		
-		if ray.is_colliding():
-			marker.visible = true
-			
-			# Ustawiamy pozycję w punkcie trafienia
-			marker.global_position = ray.get_collision_point()
-			
-			# ### TO NAPRAWIA POCHYLANIE ###
-			# Wymuszamy, by rotacja względem ŚWIATA (global) była wyzerowana.
-			# Niezależnie jak krzywo trzymasz kontroler, to nadpisze jego wpływ.
-			marker.global_rotation = Vector3.ZERO
-			
-		else:
-			marker.visible = false
-			
-	# --- 2. SKOK (bez zmian) ---
-	elif is_aiming:
-		is_aiming = false
-		marker.visible = false
-		
-		if ray.is_colliding():
-			teleport_now()
-		
-		ray.enabled = false
+    var input_vector = get_vector2("thumbstick")
+    
+    # --- STAN 1: CELOWANIE ---
+    if input_vector.length() > DEADZONE:
+        if not is_aiming:
+            is_aiming = true
+            ray.enabled = true 
+        
+        if ray.is_colliding():
+            var normal = ray.get_collision_normal()
+            
+            # ZABEZPIECZENIE: Czy celujemy w płaską podłogę?
+            # Kąt > 0.85 oznacza powierzchnię poziomą. Ściany mają ok. 0.0.
+            if normal.dot(Vector3.UP) > 0.85:
+                marker.visible = true
+                marker.global_position = ray.get_collision_point()
+                # Znacznik zawsze płasko na ziemi
+                marker.global_rotation = Vector3.ZERO
+            else:
+                # Trafiliśmy w ścianę -> Nie pokazuj znacznika
+                marker.visible = false
+        else:
+            marker.visible = false
+            
+    # --- STAN 2: SKOK (PUSZCZENIE GAŁKI) ---
+    elif is_aiming:
+        is_aiming = false
+        marker.visible = false
+        
+        # Wykonaj skok tylko jeśli laser widzi PŁASKĄ PODŁOGĘ
+        if ray.is_colliding():
+            var normal = ray.get_collision_normal()
+            if normal.dot(Vector3.UP) > 0.85:
+                teleport_now()
+        
+        ray.enabled = false
 
 func teleport_now() -> void:
-	var target = ray.get_collision_point()
-	var origin_tf := xr_origin.global_transform
-	var cam_tf := xr_camera.global_transform
-	
-	# Obliczamy przesunięcie gracza względem środka (XROrigin)
-	var cam_offset := cam_tf.origin - origin_tf.origin
-	
-	# Stabilizacja wysokości (resetujemy Y, żeby nie zapadać się w podłogę)
-	cam_offset.y = 0.0
-	
-	# Przesuwamy cały świat gracza w nowe miejsce
-	origin_tf.origin = Vector3(target.x - cam_offset.x, target.y, target.z - cam_offset.z)
-	
-	xr_origin.global_transform = origin_tf
+    var target = ray.get_collision_point()
+    var origin_tf := xr_origin.global_transform
+    var cam_tf := xr_camera.global_transform
+    
+    # Obliczamy przesunięcie, żeby wylądować stopami w celu, a nie środkiem pokoju
+    var cam_offset := cam_tf.origin - origin_tf.origin
+    cam_offset.y = 0.0
+    
+    origin_tf.origin = Vector3(target.x - cam_offset.x, target.y, target.z - cam_offset.z)
+    xr_origin.global_transform = origin_tf
