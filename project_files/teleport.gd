fcc12cada79453ff960e2055a7b3919ba98a9db5
Milosz Mackowski-Nowak <miloszmn7@gmail.com>
2026-01-22 00:43:27 +0100

    Sceneria i rece, teleportacja i chodzenie

--- project_files/teleport.gd
+++ project_files/teleport.gd
@@ -1,6 +1,5 @@
 extends XRController3D
 
-# Upewnij się, że masz te węzły jako dzieci kontrolera!
 @onready var ray: RayCast3D = $TeleportRay
 @onready var marker: MeshInstance3D = $TeleportMarker
 
@@ -13,43 +12,54 @@ func _ready() -> void:
     xr_origin = get_parent() as XROrigin3D
     xr_camera = xr_origin.get_node("XRCamera3D") as XRCamera3D
     
-    # Start: ukryte
     marker.visible = false
     ray.enabled = false 
+    
+    # --- WYMUSZENIE USTAWIEŃ (NUCLEAR OPTION) ---
+    # To naprawia błąd ludzki w inspektorze.
+    # Ustawiamy maskę kolizji na 1 (binarnie: 0001). 
+    # To oznacza: "Widzę TYLKO obiekty z Layer 1".
+    ray.collision_mask = 1 
+    # Jeśli Twoja podłoga jest na Layer 1, a obiekty na Layer 2, 
+    # to laser fizycznie PRZELECI przez obiekty.
 
 func _process(_delta: float) -> void:
     var input_vector = get_vector2("thumbstick")
     
-    # --- STAN 1: CELOWANIE ---
+    # --- CELOWANIE ---
     if input_vector.length() > DEADZONE:
         if not is_aiming:
             is_aiming = true
             ray.enabled = true 
         
         if ray.is_colliding():
+            var collider = ray.get_collider()
             var normal = ray.get_collision_normal()
             
-            # ZABEZPIECZENIE: Czy celujemy w płaską podłogę?
-            # Kąt > 0.85 oznacza powierzchnię poziomą. Ściany mają ok. 0.0.
+            # DEBUGGER PRAWDY
+            # Jeśli celujesz w skrzynię i widzisz znacznik, 
+            # spójrz w konsolę ("Output"). Jeśli wypisze nazwę skrzyni,
+            # to znaczy, że skrzynia MA włączony Layer 1.
+            # print("Celuję w: ", collider.name) 
+            
+            # Sprawdzamy kąt (dla pewności, żeby nie wchodzić na ściany granic)
             if normal.dot(Vector3.UP) > 0.85:
                 marker.visible = true
                 marker.global_position = ray.get_collision_point()
-                # Znacznik zawsze płasko na ziemi
                 marker.global_rotation = Vector3.ZERO
             else:
-                # Trafiliśmy w ścianę -> Nie pokazuj znacznika
                 marker.visible = false
         else:
             marker.visible = false
             
-    # --- STAN 2: SKOK (PUSZCZENIE GAŁKI) ---
+    # --- SKOK ---
     elif is_aiming:
         is_aiming = false
         marker.visible = false
         
-        # Wykonaj skok tylko jeśli laser widzi PŁASKĄ PODŁOGĘ
         if ray.is_colliding():
             var normal = ray.get_collision_normal()
+            # Pozwól na skok TYLKO na płaskie
             if normal.dot(Vector3.UP) > 0.85:
                 teleport_now()
         
@@ -60,7 +70,6 @@ func teleport_now() -> void:
     var origin_tf := xr_origin.global_transform
     var cam_tf := xr_camera.global_transform
     
-    # Obliczamy przesunięcie, żeby wylądować stopami w celu, a nie środkiem pokoju
     var cam_offset := cam_tf.origin - origin_tf.origin
     cam_offset.y = 0.0
     
