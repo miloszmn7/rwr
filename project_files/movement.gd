5824a56abec6869785215a050a3608da41e3ad0b
Milosz Mackowski-Nowak <miloszmn7@gmail.com>
2026-01-21 22:06:57 +0100

    Sceneria i rece, teleportacja i chodzenie

--- project_files/movement.gd
+++ project_files/movement.gd
@@ -1,51 +1,51 @@
 extends CharacterBody3D
 
+# Ustawienia prędkości
 @export var move_speed: float = 3.0
 @export var deadzone: float = 0.2
 
-# Teraz XROrigin jest dzieckiem, więc ścieżki się zmieniają!
+# UWAGA: Te ścieżki zakładają, że XROrigin3D jest dzieckiem Playera (tak jak ustaliliśmy)
 @onready var xr_origin: XROrigin3D = $XROrigin3D
 @onready var xr_camera: XRCamera3D = $XROrigin3D/XRCamera3D
 @onready var left_controller: XRController3D = $XROrigin3D/LeftController
 
 func _physics_process(_delta: float) -> void:
-	# 1. Sprawdzamy kontroler
+	# 1. Sprawdzamy czy kontroler istnieje
 	if not left_controller:
 		return
-		
+
+	# 2. Pobieramy input z gałki (obsługa thumbstick i primary)
 	var input_vector = left_controller.get_vector2("thumbstick")
 	if input_vector.length() == 0:
 		input_vector = left_controller.get_vector2("primary")
-		
-	# Martwa strefa
+	
+	# 3. Martwa strefa (jeśli nie ruszasz gałką - zatrzymaj się)
 	if input_vector.length() < deadzone:
-		velocity = Vector3.ZERO
-		move_and_slide() # Zatrzymuje nas (tarcie)
+		velocity.x = move_toward(velocity.x, 0, move_speed)
+		velocity.z = move_toward(velocity.z, 0, move_speed)
+		move_and_slide()
 		return
 
-	# 2. Obliczamy kierunki wg kamery
+	# 4. Obliczamy kierunki względem kamery
 	var forward_dir = -xr_camera.global_transform.basis.z
 	var right_dir = xr_camera.global_transform.basis.x
 
-	# Blokada latania (Y=0)
+	# Blokada latania (ignorujemy oś Y)
 	forward_dir.y = 0.0
 	right_dir.y = 0.0
 	forward_dir = forward_dir.normalized()
 	right_dir = right_dir.normalized()
 
-	# 3. Obliczamy kierunek ruchu
+	# 5. Obliczamy wektor ruchu
 	var direction = (forward_dir * input_vector.y) + (right_dir * input_vector.x)
 
-	# 4. Aplikujemy prędkość (Velocity)
+	# 6. Aplikujemy prędkość do fizycznego ciała
 	if direction.length() > 0:
-		# W CharacterBody3D używamy velocity (metry/sekundę) bezpośrednio
 		velocity.x = direction.x * move_speed
 		velocity.z = direction.z * move_speed
-		# velocity.y zostawiamy 0 (lub dodamy grawitację w przyszłości)
 	else:
 		velocity.x = 0
 		velocity.z = 0
 
-	# 5. KLUCZOWA FUNKCJA FIZYCZNA
-	# To ona odpowiada za przesuwanie i "ślizganie się" po ścianach
+	# 7. Wykonujemy ruch (TO TA FUNKCJA BLOKUJE PRZECHODZENIE PRZEZ ŚCIANY)
 	move_and_slide()
