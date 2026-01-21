802c432f3f7283201e958afac6a17d26103ea49f
Milosz Mackowski-Nowak <miloszmn7@gmail.com>
2026-01-21 21:54:51 +0100

    Sceneria i rece, teleportacja i chodzenie

--- project_files/movement.gd
+++ project_files/movement.gd
@@ -1,45 +1,51 @@
-extends XROrigin3D
+extends CharacterBody3D
 
-@export var move_speed: float = 2.5
+@export var move_speed: float = 3.0
 @export var deadzone: float = 0.2
 
-# Upewnij się, że te nazwy pasują do drzewa sceny!
-@onready var xr_camera: XRCamera3D = $XRCamera3D
-@onready var left_controller: XRController3D = $LeftController 
+# Teraz XROrigin jest dzieckiem, więc ścieżki się zmieniają!
+@onready var xr_origin: XROrigin3D = $XROrigin3D
+@onready var xr_camera: XRCamera3D = $XROrigin3D/XRCamera3D
+@onready var left_controller: XRController3D = $XROrigin3D/LeftController
 
-func _ready() -> void:
-	print("--- LOCOMOTION SCRIPT READY ---")
-	if left_controller:
-		print("Lewy kontroler znaleziony!")
-	else:
-		print("BŁĄD: Nie widzę LeftController!")
-
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
+	# 1. Sprawdzamy kontroler
 	if not left_controller:
 		return
 		
-	# Sprawdzamy surowe dane z gałki
 	var input_vector = left_controller.get_vector2("thumbstick")
-	
-	# DEBUG: Pokaż w konsoli, jeśli gałka cokolwiek nadaje
-	if input_vector.length() > 0.1:
-		print("Gałka nadaje: ", input_vector)
-	
+	if input_vector.length() == 0:
+		input_vector = left_controller.get_vector2("primary")
+		
+	# Martwa strefa
 	if input_vector.length() < deadzone:
+		velocity = Vector3.ZERO
+		move_and_slide() # Zatrzymuje nas (tarcie)
 		return
 
+	# 2. Obliczamy kierunki wg kamery
 	var forward_dir = -xr_camera.global_transform.basis.z
 	var right_dir = xr_camera.global_transform.basis.x
-	
-	# Spłaszczanie (blokada latania)
+
+	# Blokada latania (Y=0)
 	forward_dir.y = 0.0
 	right_dir.y = 0.0
 	forward_dir = forward_dir.normalized()
 	right_dir = right_dir.normalized()
-	
-	var move_direction = (forward_dir * input_vector.y) + (right_dir * input_vector.x)
-	
-	if move_direction.length() > 0:
-		# DEBUG: Pokaż, że próbujemy przesunąć gracza
-		# print("Przesuwam gracza!") 
-		global_translate(move_direction * move_speed * delta)
+
+	# 3. Obliczamy kierunek ruchu
+	var direction = (forward_dir * input_vector.y) + (right_dir * input_vector.x)
+
+	# 4. Aplikujemy prędkość (Velocity)
+	if direction.length() > 0:
+		# W CharacterBody3D używamy velocity (metry/sekundę) bezpośrednio
+		velocity.x = direction.x * move_speed
+		velocity.z = direction.z * move_speed
+		# velocity.y zostawiamy 0 (lub dodamy grawitację w przyszłości)
+	else:
+		velocity.x = 0
+		velocity.z = 0
+
+	# 5. KLUCZOWA FUNKCJA FIZYCZNA
+	# To ona odpowiada za przesuwanie i "ślizganie się" po ścianach
+	move_and_slide()
